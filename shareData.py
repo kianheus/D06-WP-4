f3edf95199eae51e874e9acd5e27b2699c7f0445
maxmckenz <57629015+maxmckenz@users.noreply.github.com>
2019-11-15 09:44:37 +0100

    Ver 1.2

--- shareData.py
+++ shareData.py
@@ -1,48 +1,48 @@
 # script to export data to and to update a file
-# to use, add
-from shareData import updateFileList
 
 # DATA SHOULD BE FORMATTED AS FOLLOWS!
-data = {"T":5,"W":0,"lambda":30}
+data = {"T": 99999, "W": 555555, "lambda": 4444444}
 
-#---------- copy code below ----------
+
+# ---------- copy code below ----------
 def updateFileList(data):
     print()
     print("Updating variables.dat...")
-    file = open('variables.dat') #open the file
+    file = open('variables.dat')  # open the file
     print("Opening the file...")
 
-    fileList = [] #making file a list
+    fileList = []  # making file a list
     for line in file:
         fileList.append(line.rstrip().split())
 
     print("Making backup...")
-    oldFile = open('oldvariables.dat','w')
+    oldFile = open('oldvariables.dat', 'w')
     for line in fileList:
-        oldFile.write(line[0]+' ' +line[1] + '\n')
+        oldFile.write(line[0] + ' ' + line[1] + '\n')
     oldFile.close()
 
-    for variableName in data: #check if variable exists
-        print("Checking if",variableName,"is already a variable...")
+    for variableName in data:  # check if variable exists
+        print("Checking if", variableName, "is already a variable...")
         existsAlready = False
         sameValue = False
         for i in range(len(fileList)):
             if str(variableName) == str(fileList[i][0]):
                 existsAlready = True
                 foundIndex = i
-                print(variableName,"already exists")
+                print(variableName, "already exists")
                 print("Checking if the value is the same...")
                 if float(data[variableName]) == float(fileList[i][1]):
                     sameValue = True
-                    print(variableName,"has the same value")
+                    print(variableName, "has the same value")
 
         if not existsAlready:
-            fileList.append([variableName,data[variableName]])
-            print(variableName,"does not exists yet, appending to the list...")
+            fileList.append([variableName, data[variableName]])
+            print(variableName, "does not exists yet, appending to the list...")
 
         elif not sameValue:
             print()
-            print("Do you want me to replace",variableName,"=",fileList[foundIndex][1],"with",fileList[foundIndex][0],"=",data[fileList[foundIndex][0]],"?")
+            print("Do you want me to replace", variableName, "=", fileList[foundIndex][1], "with",
+                  fileList[foundIndex][0], "=", data[fileList[foundIndex][0]], "?")
             choice = "a"
             while choice.lower() != "y" and choice.lower() != "n":
                 choice = input("Enter 'Y' for yes and 'N' for no: ")
@@ -52,9 +52,12 @@ def updateFileList(data):
                 fileList[foundIndex][1] = data[fileList[foundIndex][0]]
 
     print("Writing to new file...")
-    newfile = open('variables.dat','w')
+    newFile = open('variables.dat', 'w')
     for i in range(len(fileList)):
-        line = fileList[i][0] + " "+ str(fileList[i][1]) + "\n"
-        newfile.write(line)
+        line = fileList[i][0] + " " + str(fileList[i][1]) + "\n"
+        newFile.write(line)
 
     file.close()
+
+
+updateFileList(data)
